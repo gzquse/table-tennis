96c0e603ca5dad66fc55ec5a09514749daaeb5b6
scohe001 <scohe001@ucr.edu>
2022-06-11 18:06:10 -0500

    Updated image and added new About Us section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MeetingInfoComponent } from './sections/meeting-info/meeting-info.component';
 import { HeaderComponent } from './sections/header/header.component';
 import { ContactUsComponent } from './sections/contact-us/contact-us.component';
+import { AboutUsComponent } from './sections/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeetingInfoComponent,
     HeaderComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
