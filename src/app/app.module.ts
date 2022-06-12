dbb6cf71f9ac26aeae2263686363431c606cda82
scohe001 <scohe001@ucr.edu>
2022-06-11 19:13:39 -0500

    Removed annoying FontAwesome dependency and cleaned up commented code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { MeetingInfoComponent } from './sections/meeting-info/meeting-info.compo
 import { HeaderComponent } from './sections/header/header.component';
 import { ContactUsComponent } from './sections/contact-us/contact-us.component';
 import { AboutUsComponent } from './sections/about-us/about-us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { AboutUsComponent } from './sections/about-us/about-us.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sections/meeting-info/meeting-info.component.ts
+++ src/app/sections/meeting-info/meeting-info.component.ts
@@ -11,19 +11,4 @@ export class MeetingInfoComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  public getDirectionsToMaggie() : void {
-    this.getDirectionsToCoordinates("33.5990214", "-101.8862281");
-  }
-
-  private getDirectionsToCoordinates(lat: string, lon: string) : void {
-    // If it's an iPhone..
-    if( (navigator.platform.indexOf("iPhone") != -1) 
-        || (navigator.platform.indexOf("iPod") != -1)
-        || (navigator.platform.indexOf("iPad") != -1))
-         window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lon);
-    else
-         window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=" + lat + "," + lon);
-  }
-
 }
